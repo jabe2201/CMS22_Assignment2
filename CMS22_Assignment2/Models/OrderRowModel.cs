44bb860787d3247bf2ddef95c2c725e22d9fce96
jabe2201 <jacobwback@gmail.com>
2022-11-14 09:34:08 +0100

    Added Model - OrderRowModel

---
+++ CMS22_Assignment2/Models/OrderRowModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS22_Assignment2.Models
+{
+    public class OrderRowModel
+    {
+        public int CustomerId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
